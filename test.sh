fec6b2305697f5ba513f3e3b62bf6549165e65a8
DSYS course <dsys2033@fs0.cm.cluster>
2020-11-24 18:57:29 +0100

    Added test files for testing DAS things

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=defq
+#SBATCH -n 16
+#SBATCH -N 4
+#SBATCH -t 30
+
+# Load modules.
+module load python/3.6.0
+module load intel-mpi/64/5.1.2/150
+
+# Run simulation.
+#srun -n $SLURM_NTASKS --mpi=pmi2 python3 "test.py"
+
+echo "$TMPDIR"
+echo "$PWD"
+echo "$HOME"
+
+mkdir "$TMPDIR/test_folder_for_pers"
+ls "$TMPDIR" | grep "test_folder_for_pers"
+
+
