41134645071a08e71c26ab7a61d98a2656e210ab
estsaon <estsaon@gmail.com>
2020-12-08 15:02:53 +0100

    Remove .gz

--- code/simulations/halted_forest_fire.py
+++ code/simulations/halted_forest_fire.py
@@ -63,8 +63,8 @@ def run_sim(scale_factor, dataset, tmp_play, tmp_data, tmp_res):
     else:
         # Fetch the set of edges according to the rank of the process and the
         # number of partitions in use.
-        path_to_partition_file = f"{tmp_data}/{dataset}/{dataset}-{size - 1}-partitions/node{rank}.p.gz"
-        path_to_edge_file = f"{tmp_data}/{dataset}/{dataset}-{size - 1}-partitions/node{rank}.e.gz"
+        path_to_partition_file = f"{tmp_data}/{dataset}/{dataset}-{size - 1}-partitions/node{rank}.p"
+        path_to_edge_file = f"{tmp_data}/{dataset}/{dataset}-{size - 1}-partitions/node{rank}.e"
         vert_rank_mapping = read_partition_file(path_to_partition_file)
 
         # Start a ComputeNode.
