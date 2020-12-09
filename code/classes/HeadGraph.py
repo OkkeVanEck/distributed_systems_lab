6c9d2d98a287162f362560e395123fc9cbfa4997
Dennis Wind <dennisgw@live.nl>
2020-12-09 14:17:08 +0100

    headnode simple, removed threading

--- code/classes/HeadGraph.py
+++ code/classes/HeadGraph.py
@@ -1,49 +1,42 @@
 
 
 class HeadGraph:
-    def __init__(self, total_vertices, cutoff_vertices, out_e, out_v):
+    def __init__(self, total_vertices, num_sample, out_e, out_v):
         self.base_id = 0
         self.total_vertices = total_vertices;
-        self.cutoff_vertices = cutoff_vertices
-        self.prev_vertices = 0
-        self.vertices = set()
-        self.vert_in_file = set()
+        self.vertices = [set()] * num_sample
+        self.current_sample = 0;
         self.edges = set()
-        self.edges_in_file = set()
         self.out_e = out_e
         self.out_v = out_v
 
-    def get_sample_vertices(self):
-        return self.get_num_vertices() - self.prev_vertices
+    def get_num_sample_vertices(self):
+        return len(self.vertices[self.current_sample])
 
-    def get_num_vertices(self):
-        return len(self.vertices)
+    def get_vertices(self):
+        return self.vertices
 
-    def add_edge(self, vertex_from, vertex_to, base_id=None):
+    def add_edge(self, vertex_from, vertex_to, sample, base_id=None):
         if base_id == None:
             base_id = self.base_id
         src = vertex_from + base_id
         dest = vertex_to + base_id
-        self.vertices.add(src)
-        self.vertices.add(dest)
+        if sample != None:
+            self.vertices[self.current_sample].add(src)
+            self.vertices[self.current_sample].add(dest)
         self.edges.add(f"{src} {dest}")
         self.edges.add(f"{dest} {src}")
 
     def next_sample(self):
-        self.prev_vertices += self.cutoff_vertices
+        self.current_sample += 1
         self.base_id += self.total_vertices
 
-    def write_to_file(self):
-        diff_e = self.edges - self.edges_in_file
-        diff_v = self.vertices - self.vert_in_file
-
+    def write2file(self):
         with open(self.out_e, 'a') as f:
-            for e in diff_e:
+            for e in self.edges:
                 f.write(f"{e}\n")
 
         with open(self.out_v, 'a') as f:
-            for v in diff_v:
-                f.write(f"{v}\n")
-
-        self.vert_in_file = self.vert_in_file.union(diff_v)
-        self.edges_in_file = self.edges_in_file.union(diff_e)
+            for sample in vertices:
+                for v in sample:
+                    f.write(f"{v}\n")
--- code/classes/HeadNode.py
+++ code/classes/HeadNode.py
@@ -2,7 +2,6 @@ import numpy as np
 import threading
 import gzip
 import time
-import math
 
 from HeadGraph import HeadGraph
 from Enums import MPI_TAG, VertexStatus, SLEEP_TIMES
@@ -19,129 +18,82 @@ def log(message):
 
 
 class HeadNode:
-    def __init__(self, rank, n_nodes, scale_factor, total_vertices, out_v, out_e):
+    def __init__(self, rank, n_nodes, scale_factor, total_vertices, out_v, out_e, stitch=True):
         """
-        NOTE: get_vertex_rank is a function. This way we can use this class in
-                a flexible way.
         """
         self.rank = rank
         self.num_compute_nodes = n_nodes - 1
         self.total_vertices = total_vertices
-        # variable for stitching
-        self.partition_center = [None] * self.num_compute_nodes
-        self.num_centers = 0
+        self.need_stitch = stitch
+        self.self.connectivity = 0.1
 
         # scale factor 1 will be upscale sample that stays the same size
         if scale_factor < 1:
-            self.samples_remain = 1
+            self.num_sample = 1
             self.cutoff_vertices = total_vertices * scale_factor
+            self.upscale = False
         else:
-            self.samples_remain = math.floor(scale_factor * 2)
-            self.cutoff_vertices = total_vertices * (scale_factor / self.samples_remain)
-        self.sample_center = [None] * self.samples_remain # gets filled back to front
+            self.num_sample = np.floor(scale_factor * 2)
+            self.cutoff_vertices = total_vertices * (scale_factor / self.num_sample)
+            self.upscale = True
 
-        # to detect which sample a heartbeat belongs too
-        self.need_ack = [False] * self.num_compute_nodes
-
-        self.graph = HeadGraph(total_vertices, self.cutoff_vertices, out_e, out_v)
-
+        self.graph = HeadGraph(total_vertices, self.num_sample, out_e, out_v)
         self.keep_burning = True
-        self.need_stitch = True
-        self.listen_thread = threading.Thread(target=self.listen, args=())
 
     def run(self):
-        self.listen_thread.start()
-        while self.keep_burning or self.samples_remain > 0:
-            if self.need_stitch and self.num_centers == self.num_compute_nodes: # this needs to change for relight
-                self.add_stitch()
-                self.need_stitch = False
-            self.graph.write_to_file()
-            time.sleep(2)
-        self.change_center_to_final_id()
-        self.add_sample_stitch()
-        self.graph.write_to_file()
-        self.listen_thread.join()
+        for cur_sample in range(self.num_sample):
+            while(self.keep_burning):
+                tag = MPI_TAG.CONTINUE.value
+                for i in range(1, num_compute_nodes+1):
+                    data = comm.recv(source=i, tag=MPI_TAG.HEARTBEAT.value)
+                    if tag == MPI_TAG.CONTINUE.value:
+                        for [src, dest] in data:
+                            self.graph.add_edge(src, dest, cur_sample, 0)
+                            if self.done_burning():
+                                self.keep_burning = False
+                                if cur_sample < self.num_sample-1:
+                                    tag = MPI_TAG.RESET.value
+                                else:
+                                    tag = MPI_TAG.KILL.value
+                                break
+                for i in range(1, num_compute_nodes+1):
+                    comm.send(None, dest=i, tag=tag)
+            self.graph.next_sample()
+        self.stitch()
+        self.graph.write2file()
 
-    def change_center_to_final_id(self):
-        index = len(self.sample_center) - 1
-        for i in range(len(self.sample_center)):
-            self.sample_center[index - i] += i * self.total_vertices
-
-    def add_stitch(self):
-        center_list = [x for x in self.sample_center if x != None]
-        if (len(center_list)) < 2:
+    def stitch(self):
+        if not need_stitch:
             return
-        for i in range(len(center_list) - 1):
-            self.graph.add_edge(center_list[i], center_list[i+1])
-        self.graph.add_edge(center_list[-1], center_list[0])
+        if upscale:
+            stitch_nodes = list()
+            for sample in self.graph.get_vertices():
+                stitch_nodes.append(
+                    np.random.choice(list(sample),
+                        size=np.int(np.ceil(len(sample)*self.connectivity)),
+                        replace=False
+                    )
+                )
+            for i in range(self.num_sample):
+                if stitch_nodes[i].shape[0] < stitch_nodes[(i+1) % self.num_sample].shape[0]:
+                    total = stitch_nodes[i].shape[0]
+                else:
+                    total = stitch_nodes[(i+1) % self.num_sample].shape[0]
 
-    def add_sample_stitch(self):
-        center_list = [x for x in self.sample_center if x != None]
-        if (len(center_list)) < 2:
-            return
-        for i in range(len(center_list) - 1):
-            self.graph.add_edge(center_list[i], center_list[i+1], 0)
-        self.graph.add_edge(center_list[-1], center_list[0], 0)
+                edges = list(itertools.product(stitch_nodes[i], stitch_nodes[(i+1) % self.num_sample]))
+                index_edges = np.random.choice(len(edges), size=total, replace=False)
+                for j in index_edges:
+                    src, dest = edges[j]
+                    self.graph.add_edge(src, dest, None, 0)
+        else:
+            for sample in self.graph.get_vertices():
+                tmp = list(itertools.product(list(sample), list(sample)))
+                edges = list(filter(lambda x: x[0] != x[1], tmp))
+                ind_edges = np.random.choice(len(edges), size=np.int(np.ceil(len(sample)*0.5)), replace=False)
+                for j in ind_edges:
+                    src, dest = edges[j]
+                    self.graph.add_edge(src, dest, None, 0)
 
-    def send_kill(self):
-        for dest in range(1, self.num_compute_nodes+1):
-            comm.send(None, dest=dest, tag=MPI_TAG.KILL_FROM_HEAD.value)
-
-    def send_restart(self):
-        for dest in range(1, self.num_compute_nodes+1):
-            self.need_ack[dest-1] = True
-            comm.send(None, dest=dest, tag=MPI_TAG.RESET_FROM_HEAD_TAG.value)
-
-    def listen(self):
-        while (self.samples_remain > 0):
-            log(self.samples_remain)
-            while self.keep_burning:
-                self.listen_for_heartbeat()
-                self.listen_for_ack()
-                time.sleep(0.05)
-            self.samples_remain -= 1
-            self.keep_burning = True
-            if self.samples_remain > 0:
-                self.send_restart()
-                self.graph.next_sample()
-                self.partition_center = [None] * self.num_compute_nodes
-                self.need_stitch = True
-        self.keep_burning = False
-        self.send_kill()
-
-    def listen_for_heartbeat(self):
-        if comm.Iprobe(source=MPI.ANY_SOURCE,tag=MPI_TAG.HEARTBEAT.value):
-            status = MPI.Status()
-            data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI_TAG.HEARTBEAT.value, status=status)
-            sender = status.Get_source() - 1 # only works if head is rank 0
-            log(f"{data}, sender: {sender}")
-
-            # message is still received so computed node doesnt get into deadlock when sending message head doesnt want
-            if self.need_ack[sender]:
-                log("dont need this data from sender = " + str(sender))
-                return
-
-            if self.partition_center[sender] == None and len(data) > 0:
-                self.partition_center[sender] = data[0,0]
-                self.num_centers += 1
-
-            if self.sample_center[self.samples_remain-1] == None and len(data) > 0:
-                self.sample_center[self.samples_remain-1] = data[0,0]
-
-            for [src, dest] in data:
-                self.graph.add_edge(src, dest)
-                if self.done_burning():
-                    self.keep_burning = False
-                    break
-
-    def listen_for_ack(self):
-        if comm.Iprobe(source=MPI.ANY_SOURCE,tag=MPI_TAG.RESET_ACK.value):
-
-            status = MPI.Status()
-            data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI_TAG.RESET_ACK.value, status=status)
-            sender = status.Get_source() - 1 # only works if head is rank 0
-            log("found ack. sender is = " + str(sender))
-            self.need_ack[sender] = False
 
     def done_burning(self):
-        return self.graph.get_sample_vertices() >= self.cutoff_vertices
+        return self.graph.get_num_sample_vertices() >= self.cutoff_vertices
