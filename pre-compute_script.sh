a1c99b9f20661560064f09c3b0028a102dde6745
Okke van Eck <okke.van.eck@gmail.com>
2020-12-10 23:40:02 +0100

    Fixed node count

--- pre-compute_script.sh
+++ pre-compute_script.sh
@@ -21,14 +21,6 @@ case "$1" in
     ;;
 # Create jobs.
 "create_jobs")
-    # Stitching & Connectivity
-    # Halted up 3 kgs 4 stitch=True, Conn=0.1, Ring=False
-    ./manage.sh create_job sc_halted_3_4_true_false_01 halted_forest_fire.py 3 kgs 4 60 True False 0.1
-    # Halted up 3 kgs 4 stitch=True, Conn=0.01, Ring=False
-    ./manage.sh create_job sc_halted_3_4_true_false_001 halted_forest_fire.py 3 kgs 4 60 True False 0.01
-    # Halted up 3 kgs 4 stitch=False, Conn=def, Ring=def
-    ./manage.sh create_job sc_halted_3_4_false_false_0 halted_forest_fire.py 3 kgs 4 60
-
     # Scalability
     # Halted down 0.4 kgs 3 stitch=True, Conn=0.1, Ring=True
     ./manage.sh create_job scala_halted_04_3_true_true_01 halted_forest_fire.py 0.4 kgs 3 60 True True 0.1
@@ -65,6 +57,14 @@ case "$1" in
     ./manage.sh create_job wild_halted_2_9_true_true_01 wild_forest_fire.py 2 kgs 9 120 True True 0.1
     # Wild up 2 kgs 17 stitch=True, Conn=0.1, Ring=True
     ./manage.sh create_job wild_halted_2_17_true_true_01 wild_forest_fire.py 2 kgs 17 120 True True 0.1
+
+    # Stitching & Connectivity
+    # Halted up 3 kgs 5 stitch=True, Conn=0.1, Ring=False
+    ./manage.sh create_job sc_halted_3_5_true_false_01 halted_forest_fire.py 3 kgs 5 60 True False 0.1
+    # Halted up 3 kgs 5 stitch=True, Conn=0.01, Ring=False
+    ./manage.sh create_job sc_halted_3_5_true_false_001 halted_forest_fire.py 3 kgs 5 60 True False 0.01
+    # Halted up 3 kgs 5 stitch=False, Conn=def, Ring=def
+    ./manage.sh create_job sc_halted_3_5_false_false_0 halted_forest_fire.py 3 kgs 5 60
     ;;
 # Create jobs.
 "run_jobs")
